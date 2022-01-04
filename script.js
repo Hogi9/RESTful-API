e0059d5f991a06a7189df7d7c5fa6ff557d9ed3e
M Darojatun Hogi <hogi.pt@gmail.com>
2022-01-04 18:01:32 +0700

    JSON stringify untuk mengubah object jadi json

---
+++ script.js
@@ -0,0 +1,10 @@
+//object
+let mahasiswa = 
+{
+    nama : "M Darojatun Hogi",
+    npm : "18081010088",
+    email : "hogi_p@yahoo.co.id"
+}
+
+//object jadi json
+console.log (JSON.stringify(mahasiswa));
