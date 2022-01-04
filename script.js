ac873746e8cb333c4ca7dcfd036a7ad8c58613e2
M Darojatun Hogi <hogi.pt@gmail.com>
2022-01-04 18:07:25 +0700

    persiapan ajax dalam mengambil json

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-//object
-let mahasiswa = 
-{
-    nama : "M Darojatun Hogi",
-    npm : "18081010088",
-    email : "hogi_p@yahoo.co.id"
+// //object
+// let mahasiswa = 
+// {
+//     nama : "M Darojatun Hogi",
+//     npm : "18081010088",
+//     email : "hogi_p@yahoo.co.id"
+// }
+
+// //object jadi json
+// console.log (JSON.stringify(mahasiswa));
+
+
+//mengubah JSON menjadi object
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 && xhr.status == 200){
+        let mahasiswa = this.responseText;
+        console.log(mahasiswa);
+    }
 }
 
-//object jadi json
-console.log (JSON.stringify(mahasiswa));
+//true untuk asyncronus, jika tidak maka syncronus
+xhr.open('GET','data.JSON',true);
+xhr.send();
