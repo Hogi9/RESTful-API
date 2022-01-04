ba4b824bd88b7c3b48608e55f3e3dca74754d234
M Darojatun Hogi <hogi.pt@gmail.com>
2022-01-04 18:08:52 +0700

    menambahkan sedikit perintah untuk mengubah json menjadi object

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
     if(xhr.readyState == 4 && xhr.status == 200){
-        let mahasiswa = this.responseText;
+        let mahasiswa = JSON.parse(this.responseText);
         console.log(mahasiswa);
     }
 }
