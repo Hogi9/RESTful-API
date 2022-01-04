233313e0af8a678af439d3dea799611384f61d1a
M Darojatun Hogi <hogi.pt@gmail.com>
2022-01-04 18:25:31 +0700

    JQUERY ajax mengubah json menjadi object

--- script.js
+++ script.js
@@ -10,15 +10,20 @@
 // console.log (JSON.stringify(mahasiswa));
 
 
-//mengubah JSON menjadi object
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-    if(xhr.readyState == 4 && xhr.status == 200){
-        let mahasiswa = JSON.parse(this.responseText);
-        console.log(mahasiswa);
-    }
-}
+// //mengubah JSON menjadi object
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function(){
+//     if(xhr.readyState == 4 && xhr.status == 200){
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa);
+//     }
+// }
 
-//true untuk asyncronus, jika tidak maka syncronus
-xhr.open('GET','data.JSON',true);
-xhr.send();
+// //true untuk asyncronus, jika tidak maka syncronus
+// xhr.open('GET','data.JSON',true);
+// xhr.send();
+
+//Mengubah JSON menjadi object menggunakan JQUERY, data otomatis menjadi object tanpa perlu di json.parse
+$.getJSON('data.json',function(data){
+    console.log(data);
+})
