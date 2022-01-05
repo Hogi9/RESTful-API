a8ade04907150eb5675edad256934e4d9e2e34e7
M Darojatun Hogi <hogi.pt@gmail.com>
2022-01-05 17:50:32 +0700

    Ajax open API

--- Materi 3/js/script.js
+++ Materi 3/js/script.js
@@ -1,4 +1,5 @@
-$('#search-button').on('click',function(){
+function searchMovie(){
+    $('#movie-list').html('');
     $.ajax({
         url: 'http://www.omdbapi.com/',
         type :'get',
@@ -8,7 +9,47 @@ $('#search-button').on('click',function(){
             's' : $('#search-input').val()
         },
         success : function(result) {
-            console.log(result);
+            //Title, Type, Year, Poster, imdbID
+            if(result.Response == "True"){
+                movies = result.Search;
+                console.log(movies);
+                $.each(movies,function(i, data){
+                    $('#movie-list').append(`
+                        <div class = "col-md-4">
+                            <div class="card mb-3">
+                                <img src="`+ data.Poster +`" class="card-img-top">
+                                <div class="card-body">
+                                <h5 class="card-title">`+ data.Title+`</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
+                                <a href="#" class="card-link" data-bs-toggle="modal" data-bs-target="#exampleModal">See Detail</a>
+                                </div>
+                            </div>
+                    </div>
+                    `);
+                });
+
+                $('#search-input').val('');
+            }else{
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">`+result.Error+`</h1>
+                    </div>
+                `);
+            }
         }
     });
+}
+
+$('#search-button').on('click',function(){
+    searchMovie();
+});
+
+//e = event
+$('#search-input').on('keyup',function(e){
+    //13 adalah tombol enter 
+    //which bisa diganti dengan keyCode
+    //dapat dicari di web keycode.info
+    if(e.keyCode === 13){
+        searchMovie();
+    }
 });
